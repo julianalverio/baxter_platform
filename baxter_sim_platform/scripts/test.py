05927c4f0f4f1af983bf241da59224620771a7d1
Julian alverio <jalverio@mit.edu>
2018-09-22 18:22:33 -0400

    updated test script

--- baxter_sim_platform/scripts/test.py
+++ baxter_sim_platform/scripts/test.py
@@ -1,3 +1,94 @@
+import struct
+import sys
+import copy
+import numpy as np
+import rospy
+import genpy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import baxter_interface
+from baxter_interface import CHECK_VERSION
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+
+from std_msgs.msg import (
+    Header,
+)
+
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
+
+from baxter_core_msgs.msg import (
+    JointCommand,
+)
+
+import argparse
+import struct
+import sys
+import copy
+import os
+import re
+import random
+import numpy as np
+
+import rospy
+import rospkg
+
+from gazebo_msgs.srv import (
+    SpawnModel,
+    DeleteModel,
+)
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+
+from std_msgs.msg import (
+    Header,
+    Empty,
+)
+
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
+
+from baxter_core_msgs.msg import (
+    JointCommand,
+)
+
+import baxter_interface
+from baxter_interface import CHECK_VERSION
+
+import actionlib
+
+from control_msgs.msg import (
+    FollowJointTrajectoryAction,
+    FollowJointTrajectoryGoal,
+)
+from trajectory_msgs.msg import (
+    JointTrajectoryPoint,
+)
+
+import genpy
+
+from std_srvs.srv import Empty as placeholder
+
+from gazebo_msgs.srv import GetLinkState
+from gazebo_msgs.msg import ContactsState
+import csv
+
 rospy.init_node("baxter_platform_manager")
 rospy.wait_for_message("/robot/sim/started", Empty)
 moveit_commander.roscpp_initialize(sys.argv)
