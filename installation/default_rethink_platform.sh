dfa5d825c445263ae3e678925a97d828944c47e0
Julian alverio <jalverio@mit.edu>
2018-09-23 01:19:31 -0400

    Fix for installation script

--- installation/default_rethink_platform.sh
+++ installation/default_rethink_platform.sh
@@ -52,6 +52,4 @@ echo "alias velocity_server='rosrun baxter_interface joint_trajectory_action_ser
 echo "alias position_server='rosrun baxter_interface joint_trajectory_action_server.py --mode position'" >> ~/.bashrc
 # Make sure the baxter world is executable to avoid an error
 sudo chmod u+x ~/catkin_ws/src/baxter_platform/baxter_gazebo/worlds/baxter.world
-. /opt/ros/kinetic/setup.bash
-cd ~/catkin_ws; catkin_make
-. ~/.bashrc
+cd ~/catkin_ws
