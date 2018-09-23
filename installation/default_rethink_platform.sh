eeb3894fa0ee39872e730d5c023a0892628231f6
julianalverio <julian.a.alverio@gmail.com>
2018-09-22 23:28:28 -0400

    Finally fixed installation scripts??

--- installation/default_rethink_platform.sh
+++ installation/default_rethink_platform.sh
@@ -19,7 +19,6 @@ sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-source /opt/ros/kinetic/setup.bash
 sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
 mkdir ~/catkin_ws/src -p;
 # Install pip and pyassimp package
@@ -30,20 +29,29 @@ sudo -H pip install pyassimp==3.3
 sudo apt-get install gazebo7 ros-kinetic-qt-build ros-kinetic-gazebo-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-ros-control ros-kinetic-control-toolbox ros-kinetic-realtime-tools ros-kinetic-ros-controllers ros-kinetic-xacro python-wstool ros-kinetic-tf-conversions ros-kinetic-kdl-parser ros-kinetic-moveit-commander -y
 # Install MoveIt!
 sudo apt-get install ros-kinetic-moveit -y
-source /opt/ros/kinetic/setup.bash
 # Move the installation folder out of the way
 mv ~/catkin_ws/src/baxter_platform/installation ~/installation
 # Delete the custom platform to avoid name collisions
 sudo rm -rf ~/catkin_ws/src/baxter_platform 
-
 # Install Rethink Default Platform
 cd ~/catkin_ws/src
 wstool init .
 wstool merge https://raw.githubusercontent.com/RethinkRobotics/baxter_simulator/kinetic-devel/baxter_simulator.rosinstall
 wstool update
-
 # Install MoveIt! for Baxter
 cd ~/catkin_ws/src
 git clone https://github.com/ros-planning/moveit_robots.git
-
-cd ~/installation
+#Make a copy of the baxter.sh script
+cp ~/catkin_ws/src/baxter/baxter.sh ~/catkin_ws/baxter.sh
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "alias resource='source ~/.bashrc'" >> ~/.bashrc
+echo "alias sim_shell='cd ~/catkin_ws; ./baxter.sh sim'" >> ~/.bashrc
+echo "alias sim='roslaunch baxter_gazebo baxter_world.launch'" >> ~/.bashrc
+echo "alias sim_headless='roslaunch baxter_gazebo baxter_world.launch gui:=false recording:=true'" >> ~/.bashrc
+echo "alias velocity_server='rosrun baxter_interface joint_trajectory_action_server.py --mode velocity'" >> ~/.bashrc
+echo "alias position_server='rosrun baxter_interface joint_trajectory_action_server.py --mode position'" >> ~/.bashrc
+# Make sure the baxter world is executable to avoid an error
+sudo chmod u+x ~/catkin_ws/src/baxter_platform/baxter_simulator/baxter_gazebo/worlds/baxter.world
+./opt/ros/kinetic/setup.bash
+cd ~/catkin_ws; catkin_make
+.~/.bashrc
