0f7ad9c2ca9f2cd60abeeb39b1b13be8a320bb07
Julian alverio <jalverio@mit.edu>
2018-10-06 18:16:09 -0400

    Refactoring singularity environment setup script

--- installation/singularity/baxter_platform_singularity.sh
+++ installation/singularity/baxter_platform_singularity.sh
@@ -1,24 +1,31 @@
-# Set up keys for Gazebo
-apt-get remove '.*gazebo.*' '.*sdformat.*' '.*ignition-math.*' '.*ignition-msgs.*' '.*ignition-transport.*' -y
+# Get keys for Gazebo
 sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable xenial main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
-apt-get update
-# Install ROS Kinetic Desktop Full Version
+# Get keys for ROS Kinetic
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
 apt-get update
+
+# Install ROS Kinetic and other prereqs
 apt-get install ros-kinetic-desktop-full -y
 rosdep init
 rosdep update
 apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
-# Install pip and pyassimp package
+
+# Prerequisites for Baxter Platform
+apt-get install gazebo7 ros-kinetic-qt-build ros-kinetic-gazebo-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-ros-control ros-kinetic-control-toolbox ros-kinetic-realtime-tools ros-kinetic-ros-controllers ros-kinetic-xacro python-wstool ros-kinetic-tf-conversions ros-kinetic-kdl-parser ros-kinetic-moveit-commander -y
+
+# Install pip and upgrade
 apt install python-pip -y
 pip install --upgrade pip
-#pip install pyassimp==3.3
-# Prerequisites for Baxter Platfor
-apt-get install gazebo7 ros-kinetic-qt-build ros-kinetic-gazebo-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-ros-control ros-kinetic-control-toolbox ros-kinetic-realtime-tools ros-kinetic-ros-controllers ros-kinetic-xacro python-wstool ros-kinetic-tf-conversions ros-kinetic-kdl-parser ros-kinetic-moveit-commander -y
+
 # Install MoveIt!
 apt-get install ros-kinetic-moveit -y
+
+cp ~/catkin_ws/src/baxter_platform/baxter/baxter.sh ~/catkin_ws/baxter.sh
+chmod 777 ~/.ros
+chmod -R 777 ~
 
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
@@ -33,8 +40,7 @@ echo "alias set_stuff='rosparam set /robot_description_semantic -t ~/catkin_ws/s
 echo "alias load_stuff='rosparam load ~/catkin_ws/src/baxter_platform/moveit_robots/baxter/baxter_moveit_config/config/kinematics.yaml'" >> ~/.bashrc
 echo "alias setup='set_stuff; load_stuff; rosrun moveit_ros_move_group move_group'" >> ~/.bashrc
 
-cp ~/catkin_ws/src/baxter_platform/baxter/baxter.sh ~/catkin_ws/baxter.sh
-chmod 777 ~/.ros
-chmod -R 777 ~
 
+
+#pip install pyassimp==3.3
 #install pyassimp, source ~/.bashrc, catkin_make
