74b3d2753e77c7daf45b27c7e5e3430d5e2706ad
Julian Alverio <julianalverio@Julians-MacBook-Pro-2.local>
2018-10-10 02:54:24 -0400

    Rewrote dqn code

--- dev/dqn.py
+++ dev/dqn.py
@@ -26,25 +26,21 @@ from scene_generator import *
 
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
 
 
 class ReplayMemory(object):
 
-    def __init__(self, capacity):
+    def __init__(self, capacity, transition):
         self.capacity = capacity
         self.memory = []
         self.position = 0
+        self.transition = transition
 
     def push(self, *args):
         """Saves a transition."""
         if len(self.memory) < self.capacity:
             self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
+        self.memory[self.position] = self.transition(*args)
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
@@ -65,12 +61,16 @@ class DQN(nn.Module):
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
-        self.head = nn.Linear(448, 2)
+        self.head = None
+        # self.head = nn.Linear(448, 7)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
+        if self.head == None:
+            input_dim = x.size(0)*x.size(1)*x.size(2)*x.size(3)
+            self.head = nn.Linear(input_dim, 7)
         return self.head(x.view(x.size(0), -1))
 
 
@@ -83,6 +83,7 @@ class screenHandler(object):
     self.initialized = False
     self.green_x = None
     self.green_y = None
+    self.updated = True
 
   def getScreen(self):
     if not self.initialized:
@@ -108,11 +109,8 @@ class screenHandler(object):
     cropped.save("robot_image.jpg")
     assert False
     self.initialized = True
-    result = self.findGreenPixels()
-    if result == False:
-    	print("not enough green pixels found. Resetting")
-    else:
-      self.green_x, self.green_y = result
+    self.green_x, self.green_y = self.findGreenPixels()
+    self.updated = True
 
 
   def getReward_slide_right(self):
@@ -122,7 +120,7 @@ class screenHandler(object):
     return 0
 
 
-  def findGreenPixels(self, threshold=20):
+  def findGreenPixels(self):
     found = False
     while not found:
       x_coords = []
@@ -145,7 +143,7 @@ class screenHandler(object):
         return sum(x_coords)/len(x_coords), sum(y_coords)/len(y_coords) 
 
 
-  def showGreenPixels(self, threshold=20):
+  def showGreenPixels(self):
     image = self.most_recent
     newimdata = []
     whitecolor = (255, 255, 255)
@@ -162,31 +160,6 @@ class screenHandler(object):
     image.show()
     newim.show()
 
-
-
-# resize = T.Compose([T.ToPILImage(),
-#                     T.Resize(40, interpolation=Image.CUBIC),
-#                     T.ToTensor()])
-
-
-
-BATCH_SIZE = 128
-GAMMA = 0.999
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 200
-TARGET_UPDATE = 10
-
-policy_net = DQN().to(device)
-target_net = DQN().to(device)
-target_net.load_state_dict(policy_net.state_dict())
-target_net.eval()
-
-optimizer = optim.RMSprop(policy_net.parameters())
-memory = ReplayMemory(10000)
-
-
-steps_done = 0
 
 
 # reference http://sdk.rethinkrobotics.com/wiki/Hardware_Specifications#Range_of_Motion_-_Bend_Joints
@@ -211,124 +184,180 @@ def getRandomState():
   return joint_angles, joint_angles_dict
 
 
-def selectAction(state):
-    global steps_done
-    sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-      math.exp(-1. * steps_done / EPS_DECAY)
-    steps_done += 1
-    if sample > eps_threshold:
-      with torch.no_grad():
-        import pdb; pdb.set_trace()
-        return policy_net(state).max(1)[1].view(1, 1)
-    else:
-      return getRandomState()
-      # return torch.tensor(getRandomState(), device=device, dtype=torch.long)
+
+class Trainer(object):
+    # interpolation can be NEAREST, BILINEAR, BICUBIC, or LANCZOS
+    def __init__(self, interpolation=Image.BILINEAR, batch_size=128, gamma=0.999, eps_start=0.9, eps_end=0.05,
+                 eps_decay=200,
+                 target_update=10, replay_memory_size=10000, timeout=5, num_episodes=1000, resize=40,
+                 one_move_timeout=8.,
+                 move_precision=0.02, ):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.Transition = namedtuple('Transition',
+                                     ('state', 'action', 'next_state', 'reward'))
+
+        self.preprocess = T.Compose([T.Resize(resize, interpolation=interpolation),
+                                     T.ToTensor()])
+
+        self.BATCH_SIZE = batch_size
+        self.GAMMA = gamma
+        self.EPS_START = eps_start
+        self.EPS_END = eps_end
+        self.EPS_DECAY = eps_decay
+        self.TARGET_UPDATE = target_update
+
+        self.policy_net = DQN().to(self.device)
+        self.target_net = DQN().to(self.device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+
+        self.optimizer = optim.RMSprop(self.policy_net.parameters())
+        self.memory = ReplayMemory(replay_memory_size, self.Transition)
+
+        self.steps_done = 0
+
+        self.TIMEOUT = timeout  # in seconds
+        self.manager = Manager()
+        rospy.on_shutdown(self.manager.shutdown)
+        self.screen_handler = screenHandler()
+        self.num_episodes = num_episodes
+
+    def resetScene(self, sleep=False):
+        self.manager.scene_controller.deleteAllModels()
+        self.manager.scene_controller.makeModel(name='table', shape='box', roll=0., pitch=0., yaw=0.,
+                                                restitution_coeff=0., size_x=.7, size_y=1.5, size_z=.7, x=.8, y=0.,
+                                                z=.35, mass=5000, ambient_r=0.1, ambient_g=0.1, ambient_b=0.1,
+                                                ambient_a=0.1, mu1=1, mu2=1, reference_frame='')
+        self.manager.scene_controller.makeModel(name='testObject', shape='box', size_x=0.1, size_y=0.1, size_z=0.1,
+                                                x=0.8, y=0.3, z=0.75, mass=20000, mu1=1000, mu2=2000,
+                                                restitution_coeff=0.5, roll=0.1, pitch=0.2, yaw=0.3, ambient_r=0,
+                                                ambient_g=1, ambient_b=0, ambient_a=1, diffuse_r=0, diffuse_g=1,
+                                                diffuse_b=0, diffuse_a=1)
+        self.manager.scene_controller.spawnAllModels()
+        self.manager.robot_controller.moveToStart(threshold=0.1)
+        self.screen_handler.updated = False
+        if sleep:
+            rospy.sleep(1.)
+        while not self.screen_handler.updated:
+          print('Waiting for scene to re-render')
+          rospy.sleep(0.1)
+        image = self.screen_handler.most_recent
+        pixels = image.getdata()
+        green_pixels = 0
+        for idx, (r, g, b) in enumerate(pixels):
+            if g > 100 and r < 50 and b < 50:
+              green_pixels += 1
+        if green_pixels < 50:
+            self.resetScene(sleep=True)
 
 
-# This first samples a batch, concatenates
-# all the tensors into a single one, computes :math:`Q(s_t, a_t)` and
-# :math:`V(s_{t+1}) = \max_a Q(s_{t+1}, a)`, and combines them into our
-# loss. By defition we set :math:`V(s) = 0` if :math:`s` is a terminal
-# state. We also use a target network to compute :math:`V(s_{t+1})` for
-# added stability. The target network has its weights kept frozen most of
-# the time, but is updated with the policy network's weights every so often.
-# This is usually a set number of steps but we shall use episodes for
-# simplicity.
-#
 
-def optimize_model():
-    if len(memory) < BATCH_SIZE:
-        return
-    transitions = memory.sample(BATCH_SIZE)
-    # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for
-    # detailed explanation).
-    batch = Transition(*zip(*transitions))
-
-    # Compute a mask of non-final states and concatenate the batch elements
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                          batch.next_state)), device=device, dtype=torch.uint8)
-    non_final_next_states = torch.cat([s for s in batch.next_state
-                                                if s is not None])
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
-
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken
-    state_action_values = policy_net(state_batch).gather(1, action_batch)
-
-    # Compute V(s_{t+1}) for all next states.
-    next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
-    # Compute the expected Q values
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    # Compute Huber loss
-    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-
-    # Optimize the model
-    optimizer.zero_grad()
-    loss.backward()
-    for param in policy_net.parameters():
-        param.grad.data.clamp_(-1, 1)
-    optimizer.step()
+    def selectAction(self, state):
+        sample = random.random()
+        eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * \
+          math.exp(-1. * self.steps_done / self.EPS_DECAY)
+        self.steps_done += 1
+        if sample > eps_threshold:
+          with torch.no_grad():
+            import pdb; pdb.set_trace()
+            return self.policy_net(state).max(1)[1].view(1, 1)
+        else:
+          import pdb; pdb.set_trace()
+          return getRandomState()
+          # return torch.tensor(getRandomState(), device=device, dtype=torch.long)
 
 
-def resetScene(manager):
-  manager.robot_controller.moveToStart(threshold=0.1)
-  manager.scene_controller.deleteAllModels()
-  manager.scene_controller.makeModel(name='table', shape='box', roll=0., pitch=0., yaw=0., restitution_coeff=0., size_x=.7, size_y=1.5, size_z=.7, x=.8, y=0., z=.35, mass=5000, ambient_r=0.1, ambient_g=0.1, ambient_b=0.1, ambient_a=0.1, mu1=1, mu2=1, reference_frame='')
-  manager.scene_controller.makeModel(name='testObject', shape='box', size_x=0.1, size_y=0.1, size_z=0.1, x=0.8, y=0.3, z=0.75, mass=20000, mu1=1000, mu2=2000, restitution_coeff=0.5, roll=0.1, pitch=0.2, yaw=0.3, ambient_r=0, ambient_g=1, ambient_b=0, ambient_a=1, diffuse_r=0, diffuse_g=1, diffuse_b=0, diffuse_a=1)
-  manager.scene_controller.spawnAllModels()
+    # This first samples a batch, concatenates
+    # all the tensors into a single one, computes :math:`Q(s_t, a_t)` and
+    # :math:`V(s_{t+1}) = \max_a Q(s_{t+1}, a)`, and combines them into our
+    # loss. By defition we set :math:`V(s) = 0` if :math:`s` is a terminal
+    # state. We also use a target network to compute :math:`V(s_{t+1})` for
+    # added stability. The target network has its weights kept frozen most of
+    # the time, but is updated with the policy network's weights every so often.
+    # This is usually a set number of steps but we shall use episodes for
+    # simplicity.
+    #
+
+    def optimize_model(self):
+        if len(self.memory) < self.BATCH_SIZE:
+            return
+        transitions = self.memory.sample(self.BATCH_SIZE)
+        # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation).
+        batch = self.Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                              batch.next_state)), device=self.device, dtype=torch.uint8)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        next_state_values = torch.zeros(self.BATCH_SIZE, device=self.device)
+        next_state_values[non_final_mask] =self.target_net(non_final_next_states).max(1)[0].detach()
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
+
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.policy_net.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
 
 
-TIMEOUT = 5 #seconds
-manager = Manager()
-rospy.on_shutdown(manager.shutdown)
-screen_handler = screenHandler()
-num_episodes = 1000
-converter = T.ToTensor()
-for i_episode in xrange(num_episodes):
-  print "Beginning episode: ", i_episode
-  # Initialize the environment and state
-  start = rospy.Time.now()
-  resetScene(manager)
-  # state = converter(screen_handler.getScreen()).unsqueeze(0).to(device)
-  state = screen_handler.getScreen()
-  for t in count():
-    # Select and perform an action
-    action, action_dict = selectAction(state)
-    print("Started moving")
-    manager.robot_controller._left_limb.move_to_joint_positions(action_dict, timeout=8., threshold=0.02)
-    print("Done moving")
-    reward = screen_handler.getReward_slide_right()
+    def train(self):
+        for i_episode in xrange(self.num_episodes):
+          print "Beginning episode: ", i_episode
+          # Initialize the environment and state
+          start = rospy.Time.now()
+          self.resetScene(self.manager)
+          state = self.preprocess(self.screen_handler.getScreen()).unsqueeze(0).to(self.device)
+          state = self.screen_handler.getScreen()
+          for _ in count():
+            # Select and perform an action
+            action, action_dict = self.selectAction(state)
+            print("Started moving")
+            self.manager.robot_controller._left_limb.move_to_joint_positions(action_dict, timeout=self.one_move_timeout, threshold=self.move_precision)
+            print("Done moving")
 
-    # Observe new state
-    done = reward or (rospy.Time.now() - start > rospy.Duration(TIMEOUT))
-    if not reward:
-      next_state = converter(screen_handler.getScreen()).unsqueeze(0).to(device)
+            reward = self.screen_handler.getReward_slide_right()
 
-    else:
-      next_state = None
+            # Observe new state
+            done = reward or (rospy.Time.now() - start > rospy.Duration(self.TIMEOUT))
+            if not reward:
+              next_state = self.preprocess(self.screen_handler.getScreen()).unsqueeze(0).to(self.device)
+            else:
+              next_state = None
 
-    reward = torch.tensor([reward], device=device)
-    action = torch.tensor([action], device=device)
-    # next_state = torch.tensor([next_state], device=device)
+            reward = torch.tensor([reward], device=self.device)
+            action = torch.tensor([action], device=self.device)
+            # next_state = torch.tensor([next_state], device=device)
 
-    # Store the transition in memory
-    memory.push(state, action, next_state, reward)
+            # Store the transition in memory
+            self.memory.push(state, action, next_state, reward)
 
-    # Move to the next state
-    state = next_state
+            # Move to the next state
+            state = next_state
 
-    # Perform one step of the optimization (on the target network)
-    optimize_model()
-    if done:
-      break
-    # Update the target network
-    if i_episode % TARGET_UPDATE == 0:
-      target_net.load_state_dict(policy_net.state_dict())
+            # Perform one step of the optimization (on the target network)
+            self.optimize_model()
+            if done:
+              break
+            # Update the target network
+            if i_episode % self.TARGET_UPDATE == 0:
+              self.target_net.load_state_dict(self.policy_net.state_dict())
 
-torch.save(policy_net, "policy_net.pth")
-torch.save(target_net, "target_net.pth")
+        torch.save(self.policy_net, "policy_net.pth")
+        torch.save(self.target_net, "target_net.pth")
