ca0a0671335228c2dddda2d1167aaba543571c10
julianalverio <julian.a.alverio@gmail.com>
2018-10-18 07:23:05 -0700

    cleaning up pacman

--- dev/pacman.py
+++ dev/pacman.py
@@ -39,7 +39,7 @@ class ReplayMemory(object):
 
 class DQN(nn.Module):
 
-    def __init__(self, num_actions):
+    def __init__(self, num_actions, device):
         super(DQN, self).__init__()
         self.conv1 = nn.Conv2d(4, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
@@ -48,10 +48,13 @@ class DQN(nn.Module):
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
         self.head = nn.Linear(9792, num_actions)
-        self.cuda()
+        self.device = device
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x.type(torch.cuda.FloatTensor))))
+        if self.device == 'cuda':
+            x = F.relu(self.bn1(self.conv1(x.type(torch.cuda.FloatTensor))))
+        else:
+            x = F.relu(self.bn1(self.conv1(x.type(torch.FloatTensor))))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
@@ -66,8 +69,8 @@ class Trainer(object):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print("device: ", self.device)
 
-        self.policy_net = DQN(self.env.action_space.n).to(self.device)
-        self.target_net = DQN(self.env.action_space.n).to(self.device)
+        self.policy_net = DQN(self.env.action_space.n, self.device).to(self.device)
+        self.target_net = DQN(self.env.action_space.n, self.device).to(self.device)
 
         self.transition = namedtuple('Transition',
                                 ('state', 'action', 'next_state', 'reward'))
