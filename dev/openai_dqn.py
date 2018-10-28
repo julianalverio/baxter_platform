7e9d42f66e4872f22f0372bfd06e1b5de4ac44fd
julianalverio <julian.a.alverio@gmail.com>
2018-10-28 18:47:40 -0400

    Hopefully fixed memory and camera error

--- dev/openai_dqn.py
+++ dev/openai_dqn.py
@@ -93,10 +93,9 @@ class Trainer(object):
         self.out_of_bounds = False
 
 
-
+    # Grab and image, crop it, downsample and resize, then convert to tensor
     def getScreen(self):
         screen = Image.fromarray(self.env.render(mode='rgb_array')).crop((30, 100, 450, 425)).resize((105, 81), Image.NEAREST)
-        import pdb; pdb.set_trace()
         return torch.from_numpy(np.array(screen, dtype=np.float32).transpose((0, 2, 1))).unsqueeze(0).to(self.device)
 
 
