a7015d0518d5610179c53209f58f749d378b4169
julianalverio <julian.a.alverio@gmail.com>
2018-10-30 12:58:42 -0400

    bug fixes

--- dev/openai_dqn.py
+++ dev/openai_dqn.py
@@ -126,6 +126,7 @@ class Trainer(object):
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
+        import pdb; pdb.set_trace()
         state_action_values = self.policy_net(state_batch).gather(1, action_batch)
 
         next_state_values = torch.zeros(self.BATCH_SIZE, device=self.device)
@@ -255,4 +256,6 @@ try:
     completionEmail('%s done' % NUM_EPISODES)
 except Exception as e:
     import pdb; pdb.set_trace()
+    import traceback
     print(e)
+    traceback.print_tb()
