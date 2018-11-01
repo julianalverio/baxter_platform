5358cfad405112da5300be9de4e5bb9bbb55092a
julianalverio <julian.a.alverio@gmail.com>
2018-11-01 14:14:40 -0400

    iterating

--- dev/pacman.py
+++ dev/pacman.py
@@ -48,7 +48,7 @@ class DQN(nn.Module):
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
-        self.head = nn.Linear(2448, num_actions)
+        self.head = nn.Linear(1792, num_actions)
         self.device = device
 
     def forward(self, x):
