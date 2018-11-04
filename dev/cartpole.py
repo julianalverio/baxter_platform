b9d1b96bd090f2a7c3da918a48ce09fc670764a7
julianalverio <julian.a.alverio@gmail.com>
2018-11-04 15:39:41 -0500

    update pacman

--- dev/cartpole.py
+++ dev/cartpole.py
@@ -1,48 +1,37 @@
-import sys
-sys.path.insert(0, '/Users/julianalverio/venv/lib/python3.7/site-packages')
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from PIL import Image
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-
-import math
-import random
-import numpy as np
-from collections import namedtuple
-from itertools import count
-from PIL import Image
-import os
-import datetime
-import yagmail
-import pickle
+import torchvision.transforms as T
 
 
-# THIS IS A HACK SPECIFIC TO BAFFIN
-import sys
-sys.path.pop(0)
-sys.path.insert(0, '/afs/csail.mit.edu/u/j/jalverio/.local/lib/python3.5/site-packages')
-import gym
+env = gym.make('CartPole-v0').unwrapped
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-
-NUM_EPISODES = 5000
-gpu_num = "2"
-os.environ["CUDA_VISIBLE_DEVICES"] = gpu_num
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
 class ReplayMemory(object):
 
-    def __init__(self, capacity, transition):
+    def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
         self.position = 0
-        self.transition = transition
 
     def push(self, *args):
-        """Saves a transition."""
         if len(self.memory) < self.capacity:
             self.memory.append(None)
-        self.memory[self.position] = self.transition(*args)
+        self.memory[self.position] = Transition(*args)
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
@@ -51,20 +40,18 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
 
+
 class DQN(nn.Module):
 
-    def __init__(self, num_actions, device, x):
+    def __init__(self):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(4, 16, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        self.head = nn.Linear(np.product(x.size()), num_actions)
+        self.head = nn.Linear(448, 2)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
@@ -74,200 +61,137 @@ class DQN(nn.Module):
 
 
 
-class Trainer(object):
-    # warm start path points to pacman_1_4000, which is the param dict, and the actual model will be pacman_1_4000.pth
-    def __init__(self, num_episodes=5000, warm_start_path=''):
-        self.env = gym.make('CartPole-v0').unwrapped
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.transition = namedtuple('Transition',
-                                    ('state', 'action', 'next_state', 'reward'))
-        self.num_episodes = num_episodes
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
 
-        if not warm_start_path:
-            # test_state = self.getState(self.getScreen(), self.getScreen()).to(torch.device('cpu'))
-            test_state = self.getScreen().type(torch.FloatTensor).unsqueeze(0)
-            self.policy_net = DQN(self.env.action_space.n, self.device, test_state).to(self.device)
-            self.target_net = DQN(self.env.action_space.n, self.device, test_state).to(self.device)
-            torch.save(self.target_net, 'delete_initial_target_net')
+# This is based on the code from gym.
+screen_width = 600
 
-            self.batch_size = 128
-            self.gamma = 0.999
-            self.eps_start = 0.9
-            self.eps_end = 0.05
-            self.eps_decay = 200
-            self.target_update = 10
 
-            self.target_net.load_state_dict(self.policy_net.state_dict())
-            self.target_net.eval()
-            self.memory = ReplayMemory(10000, self.transition)
+def get_cart_location():
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
 
-            self.steps_done = 0
 
+def get_screen():
+    screen = env.render(mode='rgb_array').transpose(
+        (2, 0, 1))  # transpose into torch order (CHW)
+    # Strip off the top and bottom of the screen
+    screen = screen[:, 160:320]
+    view_width = 320
+    cart_location = get_cart_location()
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescare, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0).to(device)
+
+
+env.reset()
+
+
+
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
+policy_net = DQN().to(device)
+target_net = DQN().to(device)
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+optimizer = optim.RMSprop(policy_net.parameters())
+memory = ReplayMemory(10000)
+
+
+steps_done = 0
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.randrange(2)]], device=device, dtype=torch.long)
+
+
+episode_durations = []
+
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.uint8)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+num_episodes = 5000
+for i_episode in range(num_episodes):
+    env.reset()
+    last_screen = get_screen()
+    current_screen = get_screen()
+    state = current_screen - last_screen
+    for t in count():
+        action = select_action(state)
+        _, reward, done, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+
+        last_screen = current_screen
+        current_screen = get_screen()
+        if not done:
+            next_state = current_screen - last_screen
         else:
-            f = open(warm_start_path + '_params', 'r')
-            param_dict = eval(f.read())
-            self.batch_size = param_dict['batch_size']
-            self.gamma = param_dict['gamma']
-            self.eps_start = param_dict['eps_start']
-            self.eps_end = param_dict['eps_end']
-            self.eps_decay = param_dict['eps_decay']
-            self.target_update = param_dict['target_update']
-            self.steps_done = param_dict['steps_done']
-            self.memory = ReplayMemory(100000, self.transition)
+            next_state = None
 
-            self.policy_net = torch.load(warm_start_path + '_model.pth')
-            self.target_net = torch.load(warm_start_path + '_model.pth')
+        memory.push(state, action, next_state, reward)
 
-        self.optimizer = optim.Adam(self.policy_net.parameters())
+        state = next_state
 
-        self.param_dict = {
-        'batch_size' : self.batch_size,
-        'gamma': self.gamma, 
-        'eps_start' : self.eps_start,
-        'eps_end': self.eps_end,
-        'eps_decay': self.eps_decay,
-        'target_update': self.target_update
-        }
-
-
-    def rgb2gray(self, rgb):
-        img = Image.fromarray(rgb.transpose((1, 2, 0))).convert(mode='L')
-        return np.array(img, dtype=np.float64)
-
-
-    def getScreen(self):
-        # original size: 210x160x3
-        screen = np.array(Image.fromarray(self.env.render(mode='rgb_array')[0:170, :, ]).resize((80, 85))).transpose((2, 0, 1)).astype(np.uint8)
-        return torch.from_numpy(screen).to(self.device)
-
-
-    def selectAction(self, state):
-        sample = random.random()
-        eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * \
-            math.exp(-1. * self.steps_done / self.eps_decay)
-        self.steps_done += 1
-        if sample > eps_threshold:
-            with torch.no_grad():
-                return self.policy_net(state).max(1)[1].view(1, 1).type(torch.LongTensor).to(self.device)
-        else:
-            return torch.tensor([[self.env.action_space.sample()]], dtype=torch.long).to(self.device)
-
-
-
-    def optimizeModel(self):
-        if len(self.memory) < self.batch_size:
-            return
-        transitions = self.memory.sample(self.batch_size)
-        batch = self.transition(*zip(*transitions))
-
-        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                              batch.next_state)), device=self.device, dtype=torch.uint8)
-        non_final_next_states = torch.cat([s for s in batch.next_state
-                                                    if s is not None])
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
-
-        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
-
-        next_state_values = torch.zeros(self.batch_size, device=self.device)
-        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
-        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
-
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-
-        self.optimizer.zero_grad()
-        loss.backward()
-        for param in self.policy_net.parameters():
-            param.grad.data.clamp_(-1, 1)
-        self.optimizer.step()
-
-
-    def getState(self, current_screen, last_screen):
-        difference = np.array(current_screen) - np.array(last_screen)
-        gray = torch.tensor(self.rgb2gray(difference), device=self.device) / 255
-        current_screen = current_screen / 255
-        return torch.tensor(np.concatenate((current_screen.unsqueeze(0), gray.unsqueeze(0).unsqueeze(0)), axis=1).astype(np.float32), device=self.device)
-
-
-    def train(self):
-        for i_episode in range(self.num_episodes):
-            start = datetime.datetime.now()
-            print('Beginning Episode %s' % i_episode)
-            self.steps_done = 0
-            self.env.reset()
-            last_screen = self.getScreen()
-            current_screen = self.getScreen()
-            # state = self.getState(current_screen, last_screen)
-            state = last_screen.type(torch.cuda.FloatTensor).unsqueeze(0)
-            for t in count():
-                action = self.selectAction(state)
-                _, reward, done, _ = self.env.step(action.item())
-                reward = torch.tensor([reward], device=self.device)
-
-                last_screen = current_screen
-                current_screen = self.getScreen()
-                if not done:
-                    # next_state = self.getState(current_screen, last_screen)
-                    next_state = current_screen.type(torch.cuda.FloatTensor).unsqueeze(0)
-                else:
-                    next_state = None
-
-                self.memory.push(state, action, next_state, reward)
-
-                state = next_state
-
-                self.optimizeModel()
-                if done:
-                    duration = (datetime.datetime.now() - start).total_seconds()
-                    print('DURATION: %s' % duration)
-                    break
-            if i_episode % self.target_update == 0:
-                self.target_net.load_state_dict(self.policy_net.state_dict())
-            if i_episode % 500 == 0:
-                try:
-                    torch.save(self.target_net, 'pacman_%s_%s.pth' % (gpu_num, i_episode))
-                    f = open('pacman_%s_params' % i_episode, 'w+')
-                    self.param_dict['steps_done'] = self.steps_done
-                    f.write(str(self.param_dict))
-                    f.close()
-                    completionEmail('pacman %s episodes completed' % i_episode)
-                except Exception as e:
-                    completionEmail('ERROR IN PACMAN %s' % i_episode)
-                    print(e)
-                    import pdb; pdb.set_trace()
-
-    def showPacman(self, target_net_path):
-        self.target_net = torch.load(target_net_path, map_location='cpu')
-        self.env = gym.make('MsPacman-v0').unwrapped
-        self.env.reset()
-        steps_done = 0
-        done = False
-        current_screen = self.getScreen()
-        while not done:
-            self.env.render(mode='human')
-            previous_screen = current_screen
-            current_screen = self.getScreen()
-            state = self.getState(current_screen, previous_screen)
-            action = self.target_net(state).max(1)[1].view(1, 1).type(torch.LongTensor)
-            _, reward, done, _ = self.env.step(action.item())
-            steps_done += 1
-        print("Steps Done: ", steps_done)
-
-
-
-
-
-
-def completionEmail(message=''):
-  yag = yagmail.SMTP('infolab.rl.bot@gmail.com', 'baxter!@')
-  yag.send('julian.a.alverio@gmail.com', 'Training Completed', [message])
-
-
-
-
-trainer = Trainer(num_episodes=NUM_EPISODES)
-print("Trainer Initialized")
-trainer.train()
-completionEmail('%s done' % NUM_EPISODES)
-# trainer.showPacman('pacman_4000.pth')
+        optimize_model()
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
+    if i_episode % TARGET_UPDATE == 0:
+        target_net.load_state_dict(policy_net.state_dict())
