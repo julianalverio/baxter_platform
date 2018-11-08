3e440ec27f00b47c1ba14ec745ce0a9c6095a10c
julianalverio <julian.a.alverio@gmail.com>
2018-11-08 18:52:00 -0500

    iterating

--- dev/pong/pong.py
+++ dev/pong/pong.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import time
-from torch.multiprocessing import Process, Lock
+from multiprocessing import Process, Lock
 
 
 
