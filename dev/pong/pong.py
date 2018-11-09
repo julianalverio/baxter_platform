c3106abbe9197dec5fe307313ed38c6f604908ca
julianalverio <julian.a.alverio@gmail.com>
2018-11-08 21:42:32 -0500

    iterating

--- dev/pong/pong.py
+++ dev/pong/pong.py
@@ -6,9 +6,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import time
-from torch.multiprocessing import Process, Lock
-from torch import multiprocessing
-
 
 
 import math
@@ -20,7 +17,6 @@ from PIL import Image
 import os
 import datetime
 import yagmail
-import time
 import copy
 #HACK SPECIFIC TO MELVILLE
 import sys; sys.path.insert(0, '/usr/local/lib/python2.7/dist-packages')
@@ -41,13 +37,6 @@ torch.manual_seed(999)
 torch.backends.cudnn.deterministic = True
 torch.cuda.manual_seed_all(999)
 np.random.seed(999)
-
-
-def completionEmail(message=''):
-  yag = yagmail.SMTP('infolab.rl.bot@gmail.com', 'baxter!@')
-  yag.send('julian.a.alverio@gmail.com', 'Training Completed', [message])
-
-
 
 class ReplayMemory(object):
 
@@ -70,7 +59,7 @@ class ReplayMemory(object):
         return len(self.memory)
 
 class DQN(nn.Module):
-    def __init__(self, num_actions, x):
+    def __init__(self, num_actions, device, x):
         super(DQN, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
@@ -90,20 +79,20 @@ class DQN(nn.Module):
         return self.head(x.view(x.size(0), -1))
 
 
+
+
 class Trainer(object):
     def __init__(self, num_episodes=5000, warm_start_path=''):
         self.env = gym.make('Pong-v0').unwrapped
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.transition = namedtuple('Transition',
-                                        ('state', 'action', 'next_state', 'reward'))
+                                    ('state', 'action', 'next_state', 'reward'))
         self.num_episodes = num_episodes
-
-        self.memory_lock = Lock()
 
         if not warm_start_path:
             test_state = (self.getScreen() - self.getScreen()).to(torch.device('cpu'))
-            self.policy_net = DQN(self.env.action_space.n, test_state).to(self.device)
-            self.target_net = DQN(self.env.action_space.n, test_state).to(self.device)
+            self.policy_net = torch.nn.DataParallel(DQN(self.env.action_space.n, self.device, test_state)).to(self.device, non_blocking=True)
+            self.target_net = torch.nn.DataParallel(DQN(self.env.action_space.n, self.device, test_state)).to(self.device, non_blocking=True)
             torch.save(self.target_net, 'delete_initial_target_net')
 
             self.batch_size = 32
@@ -111,7 +100,7 @@ class Trainer(object):
             self.eps_start = 1.
             self.eps_end = 0.2
             # self.eps_decay = 200
-            self.decay_steps = 100000
+            self.decay_steps = 100000 #100K
             self.target_update = 1000
 
             self.target_net.load_state_dict(self.policy_net.state_dict())
@@ -119,32 +108,27 @@ class Trainer(object):
             self.memory = ReplayMemory(100000, self.transition)
 
             self.steps_done = 0
-            self.prefetch_episodes = 1000
+            self.prefetch_episodes = 10000
             print('Prefetching %s Random State Transitions...' % self.prefetch_episodes)
             self.prefetch()
             self.steps_before_optimize = 4
 
-            self.sars_exec = 0
-            self.opt_exec = 0
-            self.done = False
+        else:
+            f = open(warm_start_path + '_params', 'r')
+            param_dict = eval(f.read())
+            self.batch_size = param_dict['batch_size']
+            self.gamma = param_dict['gamma']
+            self.eps_start = param_dict['eps_start']
+            self.eps_end = param_dict['eps_end']
+            self.eps_decay = param_dict['eps_decay']
+            self.target_update = param_dict['target_update']
+            self.steps_done = param_dict['steps_done']
+            self.memory = ReplayMemory(10000, self.transition)
 
+            self.policy_net = torch.load(warm_start_path + '_model.pth')
+            self.target_net = torch.load(warm_start_path + '_model.pth')
 
-        # else:
-        #     f = open(warm_start_path + '_params', 'r')
-        #     param_dict = eval(f.read())
-        #     self.batch_size = param_dict['batch_size']
-        #     self.gamma = param_dict['gamma']
-        #     self.eps_start = param_dict['eps_start']
-        #     self.eps_end = param_dict['eps_end']
-        #     self.eps_decay = param_dict['eps_decay']
-        #     self.target_update = param_dict['target_update']
-        #     self.steps_done = param_dict['steps_done']
-        #     self.memory = ReplayMemory(10000, self.transition)
-
-            # self.policy_net = torch.load(warm_start_path + '_model.pth')
-            # self.target_net = torch.load(warm_start_path + '_model.pth')
-
-        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=0.0001)
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr = 0.0001)
 
         # self.param_dict = {
         # 'batch_size' : self.batch_size,
@@ -181,8 +165,7 @@ class Trainer(object):
         img = self.env.render(mode='rgb_array')[25:195, 8:152, :]
         img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
         img = np.array(Image.fromarray(img).resize((72, 85)))
-        img = torch.from_numpy(img/255.).unsqueeze(0).unsqueeze(0)
-        return img.to(self.device)
+        return torch.from_numpy(img/255.).unsqueeze(0).unsqueeze(0).type(torch.FloatTensor).to(self.device, non_blocking=True)
 
 
     def selectAction(self, state):
@@ -192,15 +175,13 @@ class Trainer(object):
         #     math.exp(-1. * self.steps_done / self.eps_decay)
         if sample > eps_threshold:
             with torch.no_grad():
-                return self.policy_net(state).max(1)[1].view(1, 1).type(torch.LongTensor).to(self.device)
+                return self.policy_net(state).max(1)[1].view(1, 1).type(torch.LongTensor).to(self.device, non_blocking=True)
         else:
-            return torch.tensor([[self.env.action_space.sample()]], dtype=torch.long).to(self.device)
-
+            return torch.tensor([[self.env.action_space.sample()]], dtype=torch.long).to(self.device, non_blocking=True)
 
 
 
     def optimizeModel(self):
-        self.memory_lock.acquire()
         if len(self.memory) < self.batch_size * self.steps_before_optimize:
             return
         transitions = self.memory.sample(self.batch_size * self.steps_before_optimize)
@@ -221,7 +202,6 @@ class Trainer(object):
         expected_state_action_values = (next_state_values * self.gamma) + reward_batch
 
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-        self.memory_lock.release()
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -229,22 +209,21 @@ class Trainer(object):
             param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
 
-    # do multiple state-action-reward-nextState updates and push them to the buffer in a thread-safe way
+
+    # def getState(self, current_screen, last_screen):
+    #     difference = current_screen - last_screen
+    #     return torch.cat([current_screen, difference], dim=1)
+    #
+
     def SARS(self, state):
-        transitions_to_push = []
-        for _ in range(self.steps_before_optimize):
-            action = self.selectAction(state)
-            _, reward, done, _ = self.env.step(action.item())
-            reward = torch.tensor([reward], device=self.device)
-            if not done:
-                next_state = self.getScreen()
-            else:
-                next_state = None
-            transitions_to_push.append((state, action, next_state, reward))
-        self.memory_lock.acquire()
-        for state, action, next_state, reward in transitions_to_push:
-            self.memory.push(state, action, next_state, reward)
-        self.memory_lock.release()
+        action = self.selectAction(state)
+        _, reward, done, _ = self.env.step(action.item())
+        reward = torch.tensor([reward], device=self.device)
+        if not done:
+            next_state = self.getScreen()
+        else:
+            next_state = None
+        self.memory.push(state, action, next_state, reward)
         self.steps_done += 1
         return next_state, done
 
@@ -257,59 +236,30 @@ class Trainer(object):
             completionEmail('ERROR IN PONG %s' % i_episode)
             import pdb; pdb.set_trace()
 
-    def SARSProcess(self):
-        torch.tensor(1).to(self.device)
-        self.sars_exec = 0
-        state = self.getScreen()
-        while 1:
-            while self.sars_exec >= (self.opt_exec + 2):
-                time.sleep(0.1)
-            state, done = self.SARS(state)
-            self.sars_exec += 1
-            if self.steps_done >= 1000 or done:
-                self.done = True
-                return
-
-    def optimizeModelProcess(self):
-        done = False
-        self.opt_exec = 0
-        while 1:
-            if self.done:
-                done = True
-            while self.opt_exec == self.sars_exec:
-                time.sleep(0.1)
-            print("I'm optimizing the model now", self.opt_exec)
-            self.optimizeModel()
-            if self.steps_done % self.target_update in [0,1,2,3]:
-                self.target_net.load_state_dict(self.policy_net.state_dict())
-            self.opt_exec += 1
-            if self.steps_done >= 30000:
-                print('\n\n\n\n\n\n')
-                print('\a\a\a\a')
-                print('steps done: ', self.steps_done)
-                print('total time: ', (datetime.datetime.now() - self.global_start_time).total_seconds())
-            if done:
-                return
-
-
     def train(self):
         self.steps_done = 0
-        self.global_start_time = datetime.datetime.now()
+        global_start_time = datetime.datetime.now()
         for i_episode in range(self.num_episodes+1):
             start = datetime.datetime.now()
             print('Beginning Episode %s' % i_episode)
             self.env.reset()
-            environment_process = Process(target=startEnvironmentProcess, args=(self,))
-            optimization_process = Process(target=self.optimizeModelProcess)
-            environment_process.start()
-            # optimization_process.start()
-            environment_process.join()
-            self.SARSProcess()
-            # optimization_process.join()
+            state = self.getScreen()
+            done = False
+            while not done:
+                for _ in range(self.steps_before_optimize):
+                    state, done = self.SARS(state)
+                    if self.steps_done == 30000:
+                        print((datetime.datetime.now() - global_start_time).total_seconds())
+                        import pdb;
+                        pdb.set_trace()
+                    if done: break
+                self.optimizeModel()
+
+                if self.steps_done % self.target_update == 0:
+                    self.target_net.load_state_dict(self.policy_net.state_dict())
             if i_episode % 500 == 0:
                 self.saveModel(i_episode)
             print('DURATION: %s' % (datetime.datetime.now() - start).total_seconds())
-
 
     def playback(self, target_net_path):
         self.target_net = torch.load(target_net_path, map_location='cpu')
@@ -329,16 +279,21 @@ class Trainer(object):
             steps_done += 1
         print("Steps Done: ", steps_done)
 
-def startEnvironmentProcess(trainer):
-    trainer.SARSProcess()
 
-def startOptimizerProcess(trainer):
-    trainer.optmizeModelProcess()
 
-if __name__ == '__main__':
-    multiprocessing.set_start_method('forkserver')
-    trainer = Trainer(num_episodes=NUM_EPISODES)
-    print("Trainer Initialized")
-    trainer.train()
-    completionEmail('%s done' % NUM_EPISODES)
-    # trainer.playback('pong_4000.pth')
+
+
+
+
+def completionEmail(message=''):
+  yag = yagmail.SMTP('infolab.rl.bot@gmail.com', 'baxter!@')
+  yag.send('julian.a.alverio@gmail.com', 'Training Completed', [message])
+
+
+
+
+trainer = Trainer(num_episodes=NUM_EPISODES)
+print("Trainer Initialized")
+trainer.train()
+completionEmail('%s done' % NUM_EPISODES)
+# trainer.playback('pong_4000.pth')
