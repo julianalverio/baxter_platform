02888ff2a3070fe9b87f0bfea5ab5d620160f5e0
julianalverio <julian.a.alverio@gmail.com>
2018-11-11 17:48:38 -0500

    iterating

--- dev/pong/pong.py
+++ dev/pong/pong.py
@@ -73,6 +73,7 @@ class DQN(nn.Module):
         self.head = nn.Linear(np.product(x.size()), 3)
 
     def forward(self, x):
+        import pdb; pdb.set_trace()
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
