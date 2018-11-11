63317e70ca9f72478131ae04e87376f6c0876a19
julianalverio <julian.a.alverio@gmail.com>
2018-11-11 17:39:14 -0500

    iterating

--- dev/pong/pong.py
+++ dev/pong/pong.py
@@ -213,8 +213,8 @@ class Trainer(object):
 
     def SARS(self, state):
         action = self.selectAction(state)
+        action[0][0] = self.VALID[action]
         import pdb; pdb.set_trace()
-        action = torch.tensor([self.VALID[action]]).view((1, 1)).to(self.device)
         next_state, reward, done, _ = self.env.step(action.item())
         reward = torch.tensor([reward], device=self.device)
         if not done:
