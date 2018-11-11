899a2bb6dc69f760c31eb9f89a8350c8810bc575
julianalverio <julian.a.alverio@gmail.com>
2018-11-11 18:32:36 -0500

    iterating

--- dev/pong/pong.py
+++ dev/pong/pong.py
@@ -197,9 +197,9 @@ class Trainer(object):
         reward_batch = torch.cat(batch.reward)
 
         state_action_values = self.policy_net(state_batch).gather(1, action_batch)
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         next_state_values = torch.zeros(self.batch_size*self.steps_before_optimize, device=self.device)
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
         expected_state_action_values = (next_state_values * self.gamma) + reward_batch
 
