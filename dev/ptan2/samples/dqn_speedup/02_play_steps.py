44a45dd3514cec15b737a420808c97d272ff9567
julianalverio <julian.a.alverio@gmail.com>
2018-11-11 14:56:02 -0500

    adding ptan

--- dev/ptan2/samples/dqn_speedup/02_play_steps.py
+++ dev/ptan2/samples/dqn_speedup/02_play_steps.py
@@ -2,10 +2,9 @@
 import gym
 import ptan
 import argparse
-import numpy as np
 
+import numpy as np
 import torch
-from torch.autograd import Variable
 import torch.optim as optim
 
 from tensorboardX import SummaryWriter
@@ -15,23 +14,33 @@ from lib import dqn_model, common
 
 if __name__ == "__main__":
     params = common.HYPERPARAMS['pong']
-#    params['epsilon_frames'] = 200000
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("-s", "--steps", type=int, default=1, help="Play steps to use, default=1")
+    parser.add_argument("--seed", type=int, help="Random seed to use")
     args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    if args.seed is not None:
+        np.random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        torch.cuda.manual_seed_all(args.seed)
+
+    params['batch_size'] *= args.steps
 
     env = gym.make(params['env_name'])
     env = ptan.common.wrappers.wrap_dqn(env)
+    if args.seed is not None:
+        env.seed(args.seed)
 
-    writer = SummaryWriter(comment="-" + params['run_name'] + "-basic")
-    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
-    if args.cuda:
-        net.cuda()
+    suffix = "" if args.seed is None else "_seed=%s" % args.seed
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-02_play_steps=%d%s" % (args.steps, suffix))
+    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
 
     tgt_net = ptan.agent.TargetNet(net)
     selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
     epsilon_tracker = common.EpsilonTracker(selector, params)
-    agent = ptan.agent.DQNAgent(net, selector, cuda=args.cuda)
+    agent = ptan.agent.DQNAgent(net, selector, device=device)
 
     exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
     buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params['replay_size'])
@@ -41,8 +50,8 @@ if __name__ == "__main__":
 
     with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
         while True:
-            frame_idx += 1
-            buffer.populate(1)
+            frame_idx += args.steps
+            buffer.populate(args.steps)
             epsilon_tracker.frame(frame_idx)
 
             new_rewards = exp_source.pop_total_rewards()
@@ -59,5 +68,5 @@ if __name__ == "__main__":
             loss_v.backward()
             optimizer.step()
 
-            if frame_idx % params['target_net_sync'] == 0:
+            if frame_idx % params['target_net_sync'] < args.steps:
                 tgt_net.sync()
