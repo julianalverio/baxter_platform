0026b51e94c60e4cc638741ca2277eda91e3c694
julianalverio <julian.a.alverio@gmail.com>
2018-11-13 14:39:43 -0500

    iterating

--- dev/ptan/samples/dqn_speedup/01_original.py
+++ dev/ptan/samples/dqn_speedup/01_original.py
@@ -41,11 +41,11 @@ class Trainer(object):
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=self.params['learning_rate'])
         self.reward_tracker = common.RewardTracker()
         csv_file = open('losses.csv', 'r')
-        csv_reader = csv.reader(csv_file)
-
-        self.losses = []
-        for row in csv_reader:
-            self.losses.append(row[0])
+        # csv_reader = csv.reader(csv_file)
+        self.csv_writer = csv.writer(csv_file)
+        # self.losses = []
+        # for row in csv_reader:
+        #     self.losses.append(row[0])
 
 
     def train(self):
@@ -68,16 +68,21 @@ class Trainer(object):
             self.optimizer.zero_grad()
             batch = self.buffer.sample(self.params['batch_size'])
             loss_v = common.calc_loss_dqn(batch, self.policy_net, self.target_net.target_model, gamma=self.params['gamma'], cuda=self.CUDA)
-            if loss_v.item() != float(self.losses[counter]):
-                print('FAILURE')
-                import pdb;
-                pdb.set_trace()
+            self.csv_writer.writerow([loss_v.item()])
             counter += 1
-            if counter == 1:
-                print("CHECKING NOW")
-            if counter == 2000:
-                print('ALL GOOD')
+            if counter == 5000:
+                print('DONE')
                 break
+            # if loss_v.item() != float(self.losses[counter]):
+            #     print('FAILURE')
+            #     import pdb;
+            #     pdb.set_trace()
+            # counter += 1
+            # if counter == 1:
+            #     print("CHECKING NOW")
+            # if counter == 2000:
+            #     print('ALL GOOD')
+            #     break
             loss_v.backward()
             self.optimizer.step()
 
