a269887aa6d61ecd3c94944a8bbc8b4479aa5498
julianalverio <julian.a.alverio@gmail.com>
2018-11-15 14:07:43 -0500

    iterating

--- dev/ptan/samples/dqn_speedup/pong.py
+++ dev/ptan/samples/dqn_speedup/pong.py
@@ -67,11 +67,10 @@ class DQNAgent(object):
     def __call__(self, state):
         import pdb; pdb.set_trace()
         state = torch.tensor(np.expand_dims(state, 0)).to(self.device)
-        q_v = self.dqn_model(state)
-        q = q_v.data.cpu().numpy()
-        return q
-        # actions = self.action_selector(q)
-        # return actions
+        return q_v
+        # q_v = self.dqn_model(state)
+        # q = q_v.data.cpu().numpy()
+        # return q
 
 
 class TargetNet:
