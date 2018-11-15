fc778b3b81dcaab1788c9977074779deb079f143
julianalverio <julian.a.alverio@gmail.com>
2018-11-15 14:35:53 -0500

    iterating

--- dev/ptan/samples/dqn_speedup/pong.py
+++ dev/ptan/samples/dqn_speedup/pong.py
@@ -30,6 +30,22 @@ from torch.autograd import Variable
 import os; os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 
+HYPERPARAMS = {
+        'env_name':         "PongNoFrameskip-v4",
+        'stop_reward':      18.0,
+        'run_name':         'pong',
+        'replay_size':      100000,
+        'replay_initial':   10000,
+        'target_net_sync':  1000,
+        'epsilon_frames':   10**5,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.02,
+        'learning_rate':    0.0001,
+        'gamma':            0.99,
+        'batch_size':       32
+}
+
+
 
 class DQN(nn.Module):
     def __init__(self, input_shape, n_actions, device):
@@ -58,22 +74,10 @@ class DQN(nn.Module):
 
     # input is a lazyframes object
     def forward(self, state):
-        import pdb; pdb.set_trace()
         x = torch.tensor(np.expand_dims(state, 0)).to(self.device)
         x = x.float() / 256
         x = self.conv(x).view(x.size()[0], -1)
         return self.fc(x)
-
-
-# class DQNAgent(object):
-#     def __init__(self, dqn_model, device="cpu"):
-#         self.dqn_model = dqn_model
-#         self.device = device
-#
-#     def __call__(self, state):
-#         state = torch.tensor(np.expand_dims(state, 0)).to(self.device)
-#         q_v = self.dqn_model(state)
-#         return q_v
 
 
 class TargetNet:
@@ -121,68 +125,6 @@ class EpsilonTracker:
         return max(old_epsilon, self.epsilon_final)
 
 
-# def unpack_batch(batch):
-#     states, actions, rewards, dones, last_states = [], [], [], [], []
-#     for exp in batch:
-#         state = np.array(exp.state, copy=False)
-#         states.append(state)
-#         actions.append(exp.action)
-#         rewards.append(exp.reward)
-#         dones.append(exp.last_state is None)
-#         if exp.last_state is None:
-#             last_states.append(state)       # the result will be masked anyway
-#         else:
-#             last_states.append(np.array(exp.last_state, copy=False))
-#     return np.array(states, copy=False), np.array(actions), np.array(rewards, dtype=np.float32), \
-#            np.array(dones, dtype=np.uint8), np.array(last_states, copy=False)
-#
-#
-# def calc_loss_dqn(batch, net, tgt_net, gamma, cuda=True, cuda_async=False):
-#     ExperienceFirstLast = collections.namedtuple('ExperienceFirstLast', ('state', 'action', 'reward', 'next_state'))
-#     batch = ExperienceFirstLast(*zip(*batch))
-#     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-#                                             batch.next_state)), device=torch.device('cuda'), dtype=torch.uint8)
-#     non_final_next_states = [np.array(s, copy=False) for s in batch.next_state if s is not None]
-#     import pdb; pdb.set_trace()
-#     state_batch = torch.tensor(np.array([np.array(state, copy=False) for state in batch.state], copy=False))
-#     action_batch = torch.cat([torch.tensor(int(x)).view((1,1)) for x in batch.action])
-#     reward_batch = torch.cat([torch.tensor(int(x)).view((1,1)) for x in batch.reward])
-#     state_action_values = self.policy_net(state_batch).gather(1, action_batch)
-#     next_state_values = torch.zeros(self.batch_size * self.steps_before_optimize, device=torch.device('cuda'))
-#     next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
-#     expected_state_action_values = (next_state_values * gamma) + reward_batch
-#     return nn.MSELoss()(state_action_values, expected_state_action_values)
-
-    #From my old pong:
-    # self.optimizer.zero_grad()
-    # loss.backward()
-    # for param in self.policy_net.parameters():
-    #     param.grad.data.clamp_(-1, 1)
-    # self.optimizer.step()
-
-
-    # states, actions, rewards, dones, next_states = unpack_batch(batch)
-    #
-    # states_v = torch.tensor(states)
-    # next_states_v = torch.tensor(next_states)
-    # actions_v = torch.tensor(actions)
-    # rewards_v = torch.tensor(rewards)
-    # done_mask = torch.ByteTensor(dones)
-    # if cuda:
-    #     states_v = states_v.cuda(non_blocking=cuda_async)
-    #     next_states_v = next_states_v.cuda(non_blocking=cuda_async)
-    #     actions_v = actions_v.cuda(non_blocking=cuda_async)
-    #     rewards_v = rewards_v.cuda(non_blocking=cuda_async)
-    #     done_mask = done_mask.cuda(non_blocking=cuda_async)
-    #
-    # state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
-    # next_state_values = tgt_net(next_states_v).max(1)[0]
-    # next_state_values[done_mask] = 0.0
-    #
-    # expected_state_action_values = next_state_values.detach() * gamma + rewards_v
-    # return nn.MSELoss()(state_action_values, expected_state_action_values)
-
-
 
 
 class ReplayMemory(object):
@@ -209,32 +151,22 @@ class ReplayMemory(object):
 
 class Trainer(object):
     def __init__(self):
-        self.params = common.HYPERPARAMS['pong']
+        self.params = HYPERPARAMS
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.env = gym.make('PongNoFrameskip-v4')
-
         self.env = other.common.wrappers.wrap_dqn(self.env)
+
         self.policy_net = DQN(self.env.observation_space.shape, self.env.action_space.n, self.device).to(self.device)
         self.target_net = TargetNet(self.policy_net)
-        # self.selector = actions.EpsilonGreedyActionSelector(epsilon=self.params['epsilon_start'])
         self.epsilon_tracker = EpsilonTracker(self.params)
-        # self.agent = DQNAgent(self.policy_net, device=self.device)
-        # self.exp_source = experience.ExperienceSourceFirstLast(self.env, self.agent, gamma=self.params['gamma'], steps_count=1)
-        # self.buffer = experience.ExperienceReplayBuffer(self.exp_source, buffer_size=self.params['replay_size'])
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=self.params['learning_rate'])
         self.reward_tracker = RewardTracker()
         self.transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
-        # csv_file = open('losses.csv', 'r')
-        # csv_reader = csv.reader(csv_file)
         self.memory = ReplayMemory(self.params['replay_size'], self.transition)
-        # self.losses = []
-        # for row in csv_reader:
-        #     self.losses.append(row[0])
         self.episode = 0
         self.state = self.env.reset()
         self.score = 0
         self.batch_size = self.params['batch_size']
-
 
 
     def addExperience(self):
@@ -242,14 +174,14 @@ class Trainer(object):
             action = random.randrange(self.env.action_space.n)
         else:
             action = torch.argmax(self.policy_net(self.state), dim=1)
-        next_state, reward, done, _ = self.env.step(action)
+        next_state, reward, done, _ = self.env.step(action.item())
         self.score += reward
         if done:
             self.memory.push(self.state, torch.tensor([action]), torch.tensor([reward], device=self.device), None)
             self.state = self.env.reset()
             self.episode += 1
         else:
-            self.memory.push(self.state, torch.tensor([action]), torch.tensor([reward], device=self.device), next_state)
+            self.memory.push(self.state, action), torch.tensor([reward], device=self.device), next_state)
             self.state = next_state
         return done
 
@@ -271,7 +203,6 @@ class Trainer(object):
         next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
         expected_state_action_values = (next_state_values * self.gamma) + reward_batch
         return nn.MSELoss()(state_action_values, expected_state_action_values)
-
 
 
 
@@ -305,12 +236,6 @@ class Trainer(object):
             if frame_idx % self.params['target_net_sync'] == 0:
                 self.target_net.sync()
 
-#
-# def playback(path):
-#     target_net = torch.load(path)
-#     env = gym.make('PongNoFrameskip-v4')
-#
-#
 
 
 
