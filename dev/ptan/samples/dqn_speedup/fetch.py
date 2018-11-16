c07dc72b09a00599d80453f99e51b4dd4cd33de2
julianalverio <julian.a.alverio@gmail.com>
2018-11-16 01:38:27 -0500

    iterating

--- dev/ptan/samples/dqn_speedup/fetch.py
+++ dev/ptan/samples/dqn_speedup/fetch.py
@@ -154,6 +154,7 @@ class Trainer(object):
 
     def reset(self):
         self.env.reset()
+        self.env.render()
         self.env.viewer.cam.lookat[0] = 1.
         self.env.viewer.cam.lookat[1] = 1.5
         self.env.viewer.cam.lookat[2] = 1.1
