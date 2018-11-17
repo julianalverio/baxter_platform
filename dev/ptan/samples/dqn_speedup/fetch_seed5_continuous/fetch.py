7658e45f008c48c02a479341e7468ed10b7bc743
julianalverio <julian.a.alverio@gmail.com>
2018-11-16 23:32:11 -0500

    iterating

--- dev/ptan/samples/dqn_speedup/fetch_seed5_continuous/fetch.py
+++ dev/ptan/samples/dqn_speedup/fetch_seed5_continuous/fetch.py
@@ -286,7 +286,7 @@ class Trainer(object):
             env.render(mode='human')
             time.sleep(0.1)
             action = self.convertAction(torch.argmax(target_net(state), dim=1).to(self.device))
-            env.step(action.item())
+            env.step(action)
             state = self.preprocess(env.render(mode='rgb_array'))
             score += self.getReward()
         print("Score: ", score)
