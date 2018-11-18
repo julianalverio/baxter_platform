e17e0bdb44b365846ad62bc49053de9c20b5aeb1
julianalverio <julian.a.alverio@gmail.com>
2018-11-18 00:51:35 -0500

    iterating

--- dev/dqn_speedup/fetch.py
+++ dev/dqn_speedup/fetch.py
@@ -273,11 +273,6 @@ class Trainer(object):
                 return reward, False
 
     def train(self):
-        for _ in range(200):
-            self.env.step([0,0,1,0])
-            self.env.render()
-            print(self.env.sim.data.get_site_xpos('robot0:grip')[2])
-
         frame_idx = 0
         while True:
             frame_idx += 1
