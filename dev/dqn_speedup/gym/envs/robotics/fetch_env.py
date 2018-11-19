1c1bd6e322cb0acf9b729b7737790c33b790bb17
julianalverio <julian.a.alverio@gmail.com>
2018-11-18 21:48:26 -0500

    iterating

--- dev/dqn_speedup/gym/envs/robotics/fetch_env.py
+++ dev/dqn_speedup/gym/envs/robotics/fetch_env.py
@@ -146,7 +146,7 @@ class FetchEnv(robot_env.RobotEnv):
         # Randomize start position of object.
         if self.has_object:
             object_qpos = self.sim.data.get_joint_qpos('object0:joint')
-            object_qpos[:2] = [1.37, 0.6288377]
+            object_qpos[:2] = [1.37, 0.64]
             self.sim.data.set_joint_qpos('object0:joint', object_qpos)
 
             # object_xpos = self.initial_gripper_xpos[:2]
