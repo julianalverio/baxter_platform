63bee9bfedcbed6fd4bdf6f8dafb622ab2ee78d3
julianalverio <julian.a.alverio@gmail.com>
2018-11-18 22:07:11 -0500

    iterating

--- dev/dqn_speedup/fetch.py
+++ dev/dqn_speedup/fetch.py
@@ -172,6 +172,7 @@ class Trainer(object):
     def preprocess(self, state):
         state = state[230:435, 50:460]
         Image.fromarray(state).show()
+        import pdb; pdb.set_trace()
         # state = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)
         state = cv2.resize(state, (state.shape[1]//2, state.shape[0]//2), interpolation=cv2.INTER_AREA).astype(np.float32)/256
         state = np.swapaxes(state, 0, 2)
