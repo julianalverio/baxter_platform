025a89099fed5e86ca8bb61134b6419d0169ce7a
julianalverio <julian.a.alverio@gmail.com>
2018-11-19 12:35:42 -0500

    iterating

--- dev/dqn_speedup/fetch.py
+++ dev/dqn_speedup/fetch.py
@@ -319,10 +319,10 @@ if __name__ == "__main__":
     torch.cuda.manual_seed_all(seed)
     trainer = Trainer(seed)
     print('Trainer Initialized')
-    print("Prefetching Now...")
-    # print('showing example now')
-    trainer.train()
-    # trainer.playback('fetch_seed63_900.pth')
+    # print("Prefetching Now...")
+    print('showing example now')
+    # trainer.train()
+    trainer.playback('fetch_seed22_100.pth')
 
 
 
